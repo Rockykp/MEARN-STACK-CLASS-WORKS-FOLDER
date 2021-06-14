9ad2a7dba05ddacda337b744c9a2cfc72a86b09a
Rocky Pushkaran <rockypushkarank@example.com>
2021-06-14 20:19:42 +0530

    tarnery operator program updated 14-june-2020

--- Javascript programs/decisionmaking/ternary-op.js
+++ Javascript programs/decisionmaking/ternary-op.js
@@ -1,24 +1,26 @@
-var num1=30, num2=20;
+// var num1=30, num2=20;
 
-var highest=num1>num2?num1:num2;
+// var highest=num1>num2?num1:num2;
 
-console.log(highest);
+// console.log(highest);
 
 
 // +ve or -ve
 
-var num=10;
+// var num=10;
 
-var res=num>0?"+ve":"-ve";
-console.log(res);
+// var res=num>0?"+ve":"-ve";
+// console.log(res);
 
 //odd or even
 
-var num=20;
+// var num=20;
 
-var oddoreven=num%2==0?"even":"odd";
+// var oddoreven=num%2==0?"even":"odd";
 
 
 // +ve,-ve,zero
-var num=0;
-var res=num>0?"+ve":num<0"-ve":"zero";
+
+var num=+0;
+var res=num>0?"+ve":num<0?"-ve":"zero"; 
+console.log(res);
